4e6aa53df634f8619be8ec29643863ee9a7a4cd6
Forrest Bingham <bingham_5@live.com>
2020-01-07 10:53:28 -0700

    Added Cart Context, wrapped App in Router, started Cart Component

--- farmfresh/src/App.js
+++ farmfresh/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homepage from "./components/Home";
 import './App.css';
 
+import CartContext from "./components/contexts/CartContext";
+
 function App() {
+
+  const [products] = useState(); //put Data in here
+  const [cart, setCart] = useState(
+    localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const [empty, setEmpty] = useState([]);
+
+  // const addItem = item => {
+  //   setCart(cart.filter(item => item.id !== itemId));
+  // };
+
+  function clearCart() {
+    setCart(empty);
+  }
   return (
     <div className="App">
+      <CartContext.Provider value={{cart, setCart}}>
       <Navbar/>
-      <Homepage/>
+      <Route exact path="/" component={Homepage}/>
+      </CartContext.Provider>
     </div>
   );
 }
